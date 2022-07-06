781187d2aaf926364f5a070cde7b533d8cf41404
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2022-07-07 02:12:15 +0530

    Fund disbursal

--- contracts/NFB.sol
+++ contracts/NFB.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./IPancakePair.sol";
 
-interface IBEP20{
+interface IERC20{
     function balanceOf(address _owner) external view returns (uint256);
     function decimals() external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from,address to,uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
 }
 
 interface IPancakeFactory {
@@ -14,74 +18,102 @@ interface IPancakeFactory {
 }
 
 contract NFB is ERC721,ERC721URIStorage{
+    using SafeMath for uint256;
     address owner;
     uint8 public currBatchToBeMinted = 1;
-    uint16 totalNFBSupply = 0;
+    uint16 totalNFBSupply = 1;
     uint256 public _bondPrice;
-    uint256 public totalHolders;
+    address CD3FiBUSDPairAddress;
     address CD3FiToken;
-    IBEP20 CD3FiContract;
+    IERC20 CD3FiContract;
     IPancakeFactory factory;
+    uint256 epochOfDeployment;
+    address payable sponsor;
+    address payable GuarenteeFund;
+    address payable CinemaDraft;
+    mapping (uint256 => uint256) nfbPriceInCd3Fi; //dont forget to divide the price by 100
+    IERC20 BUSDContract = IERC20(0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7);
 
     modifier onlyOwner{
         require (msg.sender == owner);
         _;
     }
 
-    constructor(address _cd3fi, address _factory) ERC721("Non Fungible Bond","NFB"){
+    constructor(address _cd3fi, address _factory, address _cd3fiBUSDPair) ERC721("Non Fungible Bond","NFB"){
         owner=msg.sender;
         CD3FiToken = _cd3fi;
-        CD3FiContract= IBEP20(_cd3fi);
+        CD3FiContract= IERC20(_cd3fi);
         factory = IPancakeFactory(_factory);
+        CD3FiBUSDPairAddress = _cd3fiBUSDPair;
+        epochOfDeployment = block.timestamp;
+        nfbPriceInCd3Fi[1] = 100000;
+        _mint(msg.sender, 1);
     }
 
     receive() external payable {}
 
-    function transferAmount(address payable _to, uint256 amnt) public payable{
+    function transferBNB(address payable _to, uint256 amnt) public payable{
         (bool sent, bytes memory data) = _to.call{value: amnt}("");
         require (sent, "Failed to send amount");
     }
 
+    function changePairAddress(address _pairAddress) public onlyOwner{
+        CD3FiBUSDPairAddress = _pairAddress;
+    }
+
     function mintNFB() public onlyOwner{
         require(totalNFBSupply <= 500, "All NFBs minted");
-        for(uint8 i= 1; i<=50; i++){
-            _mint(msg.sender, totalNFBSupply+i);
+        uint256 startId = (currBatchToBeMinted-1)*50+1;
+        uint256 incrementPercentage = 10000 + (startId - 1);
+
+        if(totalNFBSupply > 450){
+            for(uint8 i= 1; i<=50; i++){
+                _mint(sponsor, totalNFBSupply+i);
+                nfbPriceInCd3Fi[i] = incrementPercentage.mul(nfbPriceInCd3Fi[i-1]).div(10000);
+                incrementPercentage.add(1);
+            }
+        }
+        else{
+            for(uint8 i= 1; i<=50; i++){
+                _mint(msg.sender, totalNFBSupply+i);
+                nfbPriceInCd3Fi[i] = incrementPercentage.mul(nfbPriceInCd3Fi[i-1]).div(10000);
+                incrementPercentage.add(1);
+            }
         }
         currBatchToBeMinted += 1;
         totalNFBSupply += 50;
     }
 
-    function buyNFBWithoutCD3d(uint256 tokenId) public payable{
-        require(msg.value >= _bondPrice,"Insufficient funds");
-        _transferNFB(owner, msg.sender, tokenId);
-        uint256 remainingBal = msg.value.sub(_bondPrice);
-        transferAmount(payable(msg.sender), remainingBal);
+    function handlePrimarySale(address from, address to, uint256 tokenId) public payable{
+        CD3FiContract.transfer(address(this), nfbPriceInCd3Fi[tokenId]);
+        uint256 busdPriceToBeDistributed = getTokenPrice(CD3FiBUSDPairAddress, nfbPriceInCd3Fi[tokenId]).div(100);
+        //assuming this is the contract that will have BUSD to distribute
+
+        uint256 sponsorAmnt = busdPriceToBeDistributed.mul(50).div(100);
+        uint256 guarenteeFundAmnt = busdPriceToBeDistributed.mul(40).div(100);
+        uint256 cinemaDraftAmnt = busdPriceToBeDistributed.mul(10).div(100);
+        BUSDContract.transfer(CinemaDraft, cinemaDraftAmnt);
+        BUSDContract.transfer(GuarenteeFund, guarenteeFundAmnt);
+        BUSDContract.transfer(sponsor, sponsorAmnt);
     }
 
-    function buyNFBWithCD3d(uint256 tokenId) public payable{
+    function handleSecondarySale(address from, address to, uint256 tokenId) public payable{
+        CD3FiContract.transfer(address(this), nfbPriceInCd3Fi[tokenId]);
+        uint256 busdPriceToBeDistributed = getTokenPrice(CD3FiBUSDPairAddress, nfbPriceInCd3Fi[tokenId]).div(100);
+        //assuming this is the contract that will have BUSD to distribute
 
+        uint256 sponsorAmnt = busdPriceToBeDistributed.mul(8).div(100);
+        uint256 guarenteeFundAmnt = busdPriceToBeDistributed.mul(6).div(100);
+        uint256 cinemaDraftAmnt = busdPriceToBeDistributed.mul(1).div(100);
+        BUSDContract.transfer(CinemaDraft, cinemaDraftAmnt);
+        BUSDContract.transfer(GuarenteeFund, guarenteeFundAmnt);
+        BUSDContract.transfer(sponsor, sponsorAmnt);
     }
 
-    function _safeTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) internal override {
-        _transferNFB(from, to, tokenId);
-        require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-        _transferNFB(from, to, tokenId);
-    }
-
-    function _transferNFB(address from, address to, uint256 tokenId) private {
+    function _transfer(address from, address to, uint256 tokenId) internal override {
+        if(tokenId >= 451){
+            require(block.timestamp >= epochOfDeployment.add(1825 days),"Sponsor bond not matured");
+        }
         require(balanceOf(to) < 5, "Can not buy more NFBs");
         if(from == owner){
              require(CD3FiContract.balanceOf(to) >= 1000, "Not enough CD3Fi on receiver");
@@ -90,14 +122,34 @@ contract NFB is ERC721,ERC721URIStorage{
             require(CD3FiContract.balanceOf(from) >= 1000, "Not enough CD3Fi on sender");
             require(CD3FiContract.balanceOf(to) >= 1000, "Not enough CD3Fi on receiver");
         }
-        _transfer(from, to, tokenId);
+
+        if(from == owner){
+            handlePrimarySale(from, to, tokenId);
+        }
+        else{
+            handleSecondarySale(from, to, tokenId);
+        }
+        super._transfer(from, to, tokenId);
     }
 
     function getTokenPrice(address pairAddress, uint amount) private view returns(uint){
         IPancakePair pair = IPancakePair(pairAddress);
-        IBEP20 token1 = IBEP20(pair.token1());
+        IERC20 token1 = IERC20(pair.token1());
         (uint Res0, uint Res1,) = pair.getReserves();
         uint res0 = Res0*(10**token1.decimals());
-        return((amount*res0)/Res1);
+        return((amount*res0)/Res1); // return amount of token0 (BUSD) to buy amount*token1 (CD3Fi)
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721URIStorage, ERC721) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721URIStorage,ERC721)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
